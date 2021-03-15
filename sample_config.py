69850ccc0613d7a169a3dca1224529abe2868ac6
punkhere <79733447+punkhere@users.noreply.github.com>
2021-03-15 14:04:36 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
-api_id = 14371
-api_hash = "e46b6c854d2bf58a0"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+api_id = 1218324
+api_hash = "dd471e72c0f98c92720639d510a364aa"
+sudo_chat_id = -1001464356442
+owner_id = 896268975
 
 
 # Arq Api, don't make changes here
